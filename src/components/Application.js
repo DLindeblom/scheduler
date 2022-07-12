3cfe5a35943c7dbcbfb2295a8a55143f8836d079
DLindeblom <drdlindeblom@gmail.com>
2022-07-12 15:30:48 -0400

    add state to DayList in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,6 +23,9 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday")
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,8 +38,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
