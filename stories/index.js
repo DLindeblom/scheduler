82b044805be568c2e7b853c74dd60996ff34872b
DLindeblom <drdlindeblom@gmail.com>
2022-07-13 09:52:15 -0400

    add Empty component and Empty story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 
 storiesOf("Button", module)
@@ -152,5 +153,9 @@ storiesOf("Button", module)
           <Header 
             time="12pm"
           />
-
+        )
+        .add("Empty", () =>
+          <Empty 
+            onAdd={action("onAdd")}
+          />
         )
