393f33eef9148cfaead004571e3746acdd0e5b22
DLindeblom <drdlindeblom@gmail.com>
2022-07-14 10:22:21 -0400

    remove days data, make axios request to days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,6 +48,15 @@ const appointments = {
 
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+         .then(response => {
+          // console.log(response.data)
+          setDays([...response.data])
+         })
+  }, [])
 
   const [day, setDay] = useState("Monday")
 
