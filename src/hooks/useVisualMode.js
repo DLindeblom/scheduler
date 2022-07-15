f7426fef765e7588ca366cb3e0aebe9d25f97023
DLindeblom <drdlindeblom@gmail.com>
2022-07-15 09:21:42 -0400

    write test to transition with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,16 +1,28 @@
 import { React, useState } from "react";
 
+// custom hook to move around views
 export default function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(mode) { setMode(mode), history.push(mode); };
-  function back() {
+  function transition(mode, replace = false) {
+    //skiping a mode
+    if (replace) {
+      history.pop();
+      setMode(mode);
+    }
+    //regular mode setting: moving foward one at a time
+    setMode(mode);
+    history.push(mode);
+  };
 
+  function back() {
+    //mode cant go past initial
     if (history.length === 1) {
       setMode(initial);
     } else {
+    //moving back one at a time
       history.pop();
       let lastValue = history[history.length - 1];
       setMode(lastValue);
