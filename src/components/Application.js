e4fdee382b000991d12ab67a72f146f9ea78c890
DLindeblom <drdlindeblom@gmail.com>
2022-07-18 10:22:11 -0400

    console log shows id and interview object in appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -30,6 +30,10 @@ export default function Application(props) {
     })
   }, []);
 
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+  }
+
   const dailyAppointments = getAppointmentsForDay(state, state.day)
 
   const schedule = dailyAppointments.map((appointment) => {
@@ -42,6 +46,7 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,7 +10,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
 
-  const { time, interview, interviewers } = props;
+  const { id, time, interview, interviewers, bookInterview } = props;
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE"
@@ -18,13 +18,24 @@ export default function Appointment(props) {
     interview ? SHOW : EMPTY
   );
   
+  function save(name, interviewer) {
+    const saveInterview = {
+      student: name,
+      interviewer
+    };
+   bookInterview(id, saveInterview)
+  }
 
   return (
     <article className="appointment">
       <Header time={time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
       {mode === SHOW && <Show interviewer={interview.interviewer} student={interview.student}/>}
-      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back(EMPTY)}/>}
+      {mode === CREATE && <Form 
+                            interviewers={interviewers}  
+                            onCancel={() => back(EMPTY)}
+                            onSave={save}
+                          />}
 
     </article>
   );
