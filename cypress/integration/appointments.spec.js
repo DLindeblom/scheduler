081f09c7ff26cf709af762e1f87a08bf376032fe
DLindeblom <drdlindeblom@gmail.com>
2022-07-20 19:34:06 -0400

    refactor test to use before each

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,26 @@
+
+
 describe("Appointments", () => {
+
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  });
+
   it("should book an interview", () => {
-    cy.visit("/")
-    cy.contains("Monday")
     cy.get("[alt=Add]")
-    cy.first()
-    cy.click()
-  })
-})
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  });
+
+});
